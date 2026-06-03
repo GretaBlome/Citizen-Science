2994db6147ed1ed035ae0709e7160fe3ee5d48fc
Greta Blome <gretablome@MacBook-Air-von-Greta.local>
2026-06-03 16:54:23 -0400

    Fix PayPal donation button

--- app/turtle-id/page.tsx
+++ app/turtle-id/page.tsx
@@ -264,7 +264,31 @@ export default function TurtleIdPage() {
     method="post"
     target="_top"
   >
-    ...
+   <form
+  action="https://www.paypal.com/donate"
+  method="post"
+  target="_top"
+>
+  <input
+    type="hidden"
+    name="hosted_button_id"
+    value="LL7SDKNJWQTA6"
+  />
+
+  <input
+    type="image"
+    src="https://www.paypalobjects.com/en_US/i/btn/btn_donate_LG.gif"
+    name="submit"
+    title="PayPal - The safer, easier way to pay online!"
+    alt="Donate with PayPal button"
+  />
+
+  <img
+    alt=""
+    src="https://www.paypal.com/en_LC/i/scr/pixel.gif"
+    width="1"
+    height="1"
+  />
   </form>
 </div>
 
