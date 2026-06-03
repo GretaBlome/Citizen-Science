11423e0b575d9bc746fcd40d2130d30d4abcf75a
Greta Blome <gretablome@MacBook-Air-von-Greta.local>
2026-06-03 17:00:32 -0400

    Fix PayPal donation button

--- app/turtle-id/page.tsx
+++ app/turtle-id/page.tsx
@@ -264,11 +264,6 @@ export default function TurtleIdPage() {
     method="post"
     target="_top"
   >
-   <form
-  action="https://www.paypal.com/donate"
-  method="post"
-  target="_top"
->
   <input
     type="hidden"
     name="hosted_button_id"
